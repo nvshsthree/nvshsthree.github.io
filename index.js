a37318c02065c5f03f8bad66e94e75aada51d90f
what? <jack.weller@hotmail.com>
2022-11-18 21:09:11 +1000

    Yesh

---
+++ index.js
@@ -0,0 +1,42 @@
+function load() {
+    var youtube = false;
+    var facebook = false;
+    var allow = false;
+
+    fetch('https://youtube.com/').then( response => {
+        if(response.ok){
+            youtube = true;
+        };
+        if(!response.ok){
+            youtube = false;
+        };
+    });
+    fetch('https://facebook.com/').then( response => {
+        if(response.ok){
+            facebook = true;
+        };
+        if(!response.ok){
+            facebook = false;
+        };
+    });
+
+    console.log(youtube);
+    console.log(facebook);
+
+    if(youtube == false && facebook == false){
+        allow = true;
+    };
+    if(youtube == true && facebook == false){
+        allow = false;
+    };
+    if(youtube == true && facebook == true){
+        allow = true;
+    };
+
+    if(allow = true){
+        location.replace("https://nvshs.github.io/portal");
+    };
+    if(allow == false){
+        location.replace("https://nvshs.github.io/infosite");
+    };
+}
