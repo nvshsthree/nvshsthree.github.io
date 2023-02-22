684a3e8ae900afa4123c63701bb9f7b7f8427ce3
leaf456 <leafkids4@gmail.com>
2023-02-22 20:31:19 +1000

    Allow offline

--- index.js
+++ index.js
@@ -1,44 +1,14 @@
+function doneswstuff() {
+	location.replace("/portal");
+}
 function load() {
-    var youtube = false;
-    /*var facebook = false;
-    var allow = false;*/
-    location.replace("/portal");
-    /*fetch('https://simplecorsoverwrite.chowders.workers.dev/?https://example.com').then().then( response => {
-        if(response.ok){
-            location.replace("https://nvshs.github.io/infosite");
-        } else {
-            location.replace("https://nvshs.github.io/portal");
-        }
-    });*/
-    /*fetch('https://facebook.com/').then( response => {
-        if(response.ok){
-            facebook = true;
-        };
-        if(!response.ok){
-            facebook = false;
-        };
-    });*/
-
-    //console.log(youtube);
-    //console.log(facebook);
-
-    /*if(youtube == false && facebook == false){
-        allow = true;
-    };
-    if(youtube == true && facebook == false){
-        allow = false;
-    };
-    if(youtube == true && facebook == true){
-        allow = true;
-    };*/
-    if (youtube) {
-        allow = false   
-    }
-
-    if(allow = true){
-        //location.replace("https://nvshs.github.io/portal");
-    };
-    if(allow == false){
-        //location.replace("https://nvshs.github.io/infosite");
-    };
+	if('serviceWorker' in navigator) {
+		let registration;
+			navigator.serviceWorker.register("sw.js").then(function(finalworker) {
+				console.log("registerd")
+				setTimeout(doneswstuff, 300)
+			})
+	} else {
+		doneswstuff()
+	}
 }
---
+++ sw.js
@@ -0,0 +1,46 @@
+const precache = ['/portal/', '/index.js', '/index.html', '/portal/index.js', '/portal/styles.css', '/portal/psiphon3.exe', '/portal/README.md', '/portal/blooket', '/portal/blooket/index.js', '/sound', '/sound/README.md', '/sound/styles.css', "/sound/sounds/airhorn.mp3",
+"/sound/sounds/aughhh.mp3",
+"/sound/sounds/bruh.mp3",
+"/sound/sounds/dog-doing.mp3",
+"/sound/sounds/emotional-damage.mp3",
+"/sound/sounds/error.mp3",
+"/sound/sounds/fail.mp3",
+"/sound/sounds/fart.mp3",
+"/sound/sounds/finals.mp3",
+"/sound/sounds/fortnite-death.mp3",
+"/sound/sounds/goofy-agg.mp3",
+"/sound/sounds/hello-there.mp3",
+"/sound/sounds/mario-jump.mp3",
+"/sound/sounds/oof.mp3",
+"/sound/sounds/ping.mp3",
+"/sound/sounds/sad-violin.mp3",
+"/sound/sounds/send-to-jesus.mp3",
+"/sound/sounds/taco-bell.mp3",
+"/sound/sounds/vine-boom.mp3",
+"/sound/sounds/virus.mp3",
+"/sound/sounds/we-do-not-care.mp3",
+"/sound/sounds/wow.mp3",
+"/sound/sounds/yeet.mp3"];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('my-cache').then(cache => {
+      return cache.addAll(precache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open('my-cache').then(cache => {
+      return fetch(event.request)
+        .then(response => {
+          cache.put(event.request, response.clone());
+          return response;
+        })
+        .catch(() => {
+          return cache.match(event.request);
+        });
+    })
+  );
+});
